849bdeb80e9c2c32d1f4e9455df950593d1f02e4
Vick Alexa <142795784+Vickabu@users.noreply.github.com>
2024-10-16 18:45:50 +0200

    setup cypress

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginJest from "eslint-plugin-jest";
+import pluginCypress from "eslint-plugin-cypress";
 
 export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.jest } } },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.jest, ...globals.node },
+    },
+  },
   pluginJs.configs.recommended,
   {
     plugins: {
@@ -11,6 +16,21 @@ export default [
     },
     rules: {
       ...pluginJest.configs.recommended.rules,
+    },
+  },
+  {
+    files: ["**/*.cy.js", "cypress.config.js"],
+    languageOptions: {
+      globals: {
+        "cypress/globals": true,
+      },
+    },
+    plugins: {
+      cypress: pluginCypress,
+    },
+    rules: {
+      "cypress/no-unnecessary-waiting": "off",
+      "no-unused-vars": "off",
     },
   },
 ];
