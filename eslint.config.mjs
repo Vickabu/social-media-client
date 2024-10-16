9e661418686a81d0004fdb74143b4f753674acd5
Vick Alexa <142795784+Vickabu@users.noreply.github.com>
2024-10-16 15:55:30 +0200

    installs and config jest and babel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import pluginJest from "eslint-plugin-jest"; // Importer Jest-plugin som objekt
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    plugins: {
+      jest: pluginJest,
+    },
+    rules: {
+      ...pluginJest.configs.recommended.rules,
+    },
+  },
 ];
